a136607f5fec58b7015ef510d45b2b14eaf4ffeb
ivan izarrualde <imizarrualde@gmail.com>
2021-11-10 23:32:49 -0300

    agrego de carrito de compras y boton de subscripcion CLASE 12

--- script.js
+++ script.js
@@ -1,66 +1,107 @@
+//JQUERY EVENTOS
+let carrito = [];
 
-//TAG ETIQUETA
-let parrafos = document.getElementsByTagName("p");
-let ingresadoPrompt = prompt("Ingrese su nombre aqui")
-parrafos[2].innerText = "Hola " + ingresadoPrompt + ", bienvenido/a"; 
-
-//ARRAYS o LISTAS
-const listaDeProductos = [ "Menu del día", "1 Ñoquis de papa con salsa fileto ($125)", " 2 Fideos de verdura con salsa bolognesa ($70)", "3 Ravioles de ricota con salsa bolognesa ($50)", "4 Agnolotis de jamon y queso con salsa bolognesa ($100)"];
-alert(listaDeProductos.join(" - "))
-
-for (let i = 0; i < listaDeProductos.length; i++) //LENGTH CUENTA CANTIDAD DE ELEMENTOS UNO ABAJO DEL OTRO  
-{
-    console.log(listaDeProductos[i]);
-}
-console.log(listaDeProductos) 
-
-
-let listaAString = listaDeProductos.toString(); //TODO EN UN SOLO toSTRING 
-console.log(listaAString); 
- let productoElegido = parseInt(prompt("Seleccione el numero de producto")); 
-
-/* const productos = [{ id: 1, producto: "Ñoquis", salsa : "fileto", relleno : "Papa" , precio: 125 },
-    { id: 2, producto: "Fideo", salsa : "bolognesa", relleno : "Verdura" , precio: 70 },
-    { id: 3, producto: "Ravioles",salsa : "bolognesa", relleno : "Ricota" , precio: 50 },
-    { id: 4, producto: "Agnolotis", salsa : "bolognesa", relleno : "Jamon y queso" , precio: 100 }
-]; */
-const productos = [{ id: 1, producto: "Ñoquis de papa con salsa fileto" , precio: 125 },
-    { id: 2, producto: "Fideos de verdura con salsa bolognesa" , precio: 70 },
-    { id: 3, producto: "Ravioles de ricota con salsa bolognesa", precio: 50 },
-    { id: 4, producto: "Agnolotis de jamon y queso con salsa bolognesa" , precio: 100 }
-];
+$(document).ready(function() {
+    //cambios en el dom
+    $("#fila_prueba").css({ background: "red", color: "white" });
+    $("#boton").prepend("<button class='btn btn-warning' id='btnSuscrip'>Suscribirse a Prato</button>"); //poner como primer hijo
+    $("#btnSuscrip").click(function() {
+        suscribir();
+    });
+    //llamada a renderizar
+    renderizarProductos();
+    //selector y ordenamiento
+    $("#miSeleccion option[value='pordefecto']").attr("selected", true); //selected es el valor de por defecto (cada vez que se cargue la page sera este el valor por defecto)
+    $("#miSeleccion").change(function() {
+        ordenar();
+    });
+});
 
 
-//FILTRER
-const elegido = productos.filter(producto => producto.id == productoElegido);
-console.log(elegido); 
-
-
-let descuento = prompt("Ingrese cupon de descuento (10%)");
-
-//Correciones (elegido[0]) y (descuento.toLowerCase() == "si")
-if (descuento == "Prato123" || "prato123" || "PRATO123" ){
-    alert("El precio final con descuento será de: " + (elegido[0].precio - (elegido[0].precio / 10)))
-} else {
-    alert("No tiene descuento, su precio final es de: " + elegido[0].precio)
+function ordenar() {
+    let seleccion = $("#miSeleccion").val(); //para tomar el valor del selector
+    console.log(seleccion);
+    //sort = ordena los elementos
+    if (seleccion == "menor") {
+        //ordenamos el array de productos por menor precio
+        productos.sort(function(a, b) { return a.precio - b.precio });
+    } else if (seleccion == "mayor") {
+        //ordenamos el array de productos por mayor precio
+        productos.sort(function(a, b) { return b.precio - a.precio });
+    } else if (seleccion == "alfabetico") {
+        productos.sort(function(a, b) {
+            return a.producto.localeCompare(b.producto);//hace que quede por orden alfabetico
+        });
+    }
+    $("li").remove();//borrar los li que ya teniamos, se renderizan nuevamente (averiguar bien la funcion del remove)
+    renderizarProductos();//para que los vuelva a renderizar
 }
 
-//DOM
-let pepe = document.getElementsByTagName("p");
-let numCuotas = parseInt(prompt("Ingrese cantida de cuotas"))
-let productoJson = "Producto elegido: " + elegido[0].producto; 
-let precioJson = "Precio por cuota: " + (elegido[0].precio / numCuotas).toFixed(2);
-const AJSONProducto = JSON.stringify(productoJson); 
-const AJSONPrecio = JSON.stringify(precioJson); 
-pepe[4].innerText = JSON.parse(AJSONProducto);
-pepe[5].innerText = JSON.parse(AJSONPrecio);  
 
-//local storage
-localStorage.setItem ("Producto elegido", AJSONProducto);
-localStorage.setItem ("Precio por cuota", AJSONPrecio);
+function suscribir() {
+    $("#suscripcion").append(`
+    <h4>Suscribite a Prato para obtener mejores descuentos</h4>
+    <form id="miFormulario">
+    <input type="text" id="email" placeholder="Aqui tu email">
+    <button type="submit" class="btn btn-warning">Suscribite ahora</button>
+    </form>`);
+
+    //evento submit
+    $("#miFormulario").submit(function(e) {
+        //prevenir el comportamiento por defecto
+        e.preventDefault();
+        //aca una buena validacion
+        Swal.fire(
+            'Nueva suscripcion',
+            $("#email").val(),
+            'success'
+        )
+        //una ves suscripto vacia todo el espacio de subscripcion
+        $("#suscripcion").empty();
+    });
+}
 
 
+function renderizarProductos() {
+    for (const producto of productos) {
+        $(".milista").append(`<li class="list-group-item paddingSpace"><h3>ID: ${producto.id}</h3>
+        <img  src=${producto.foto} width="300" height="250">
+        <p> Producto: ${producto.producto}</p>
+        <b> $ ${producto.precio}</b>
+        <button class='btn btn-danger boxmodCentrar' id='btn${producto.id}'>Comprar</button>
+        </li>`);//CREA BTN CON CLASS Y ID
+        //Eventos para cada boton
+        $(`#btn${producto.id}`).on('click', function() {
+            agregarAlCarrito(producto); //mando completo el objeto literal
+        });
+    }
+}
 
+function agregarAlCarrito(productoNuevo) {
+    carrito.push(productoNuevo);
+    console.log(carrito);
+    //sweet alert
+    Swal.fire(
+        'Nuevo producto agregado al carrito',
+        productoNuevo.producto,
+        'success'
+    );
+    localStorage.setItem("miCarrito", JSON.stringify(carrito));
+}
+
+function agregarAlCarrito(productoNuevo) {
+    carrito.push(productoNuevo);
+    console.log(carrito);
+    //sweet alert
+    localStorage.setItem("miCarrito", JSON.stringify(carrito));
+}
+
+
+const productos = [{ id: 1, producto: "Ñoquis de papa con salsa fileto" ,foto: "./assets/img/ñoquisSalsa.jpg" ,precio: 125 },
+{ id: 2, producto: "Fideos de verdura con salsa bolognesa" ,foto: "./assets/img/fideosVerdes.jpg", precio: 70 },
+{ id: 3, producto: "Ravioles de ricota con salsa bolognesa",foto: "./assets/img/raviolesSalsa.jpg", precio: 50 },
+{ id: 4, producto: "Agnolotis de jamon y queso con salsa bolognesa" ,foto: "./assets/img/pastas5.jpg ", precio: 100 }
+]; 
 
 //tabla con DOM
 let tabla = document.createElement("table");
@@ -74,8 +115,6 @@ for (const producto of productos) {
     fila.innerHTML = `<td> ${producto.id}</td>
     <td>${producto.producto}</td>
     <td><b>$ ${producto.precio}</b></td>`;
-/*  <td>${producto.salsa}</td>
-    <td>${producto.relleno}</td> */
     
     //guarda la final
     tablaBody.appendChild(fila);
@@ -88,39 +127,11 @@ tabla.appendChild(tablaBody);
 document.getElementById("inferior").appendChild(tabla);
 
 
-
-
-
-
-//OPCION 2
-let boton = document.getElementById("miBoton");
-//boton.onclick = () => { console.log("Click registrado!"); }
-boton.addEventListener ("click", () => console.log("click"))
-//evento donde pasas el mouse
-boton.onmouseover = () => { console.log("No me toques"); } 
-
-
-
-function interactuar(){
-    let nombre = prompt("ingrese tu nombre");
-    let fecha = new Date().toLocaleDateString();
-    alert("Hola " + nombre + " hoy es " + fecha);
-} 
-
-//Evento de teclado desde html detectando el ENTER (Sirve para detectar la tecla presionada, en este caso el enter)
-function capturarEnter(evento) {
-    //codigo 13 = enter
-    if (evento.which == 13 || evento.keyCode == 13) {
-        evento.preventDefault()
-        alert("Presionaste ENTER");
-    }
-}
-
-//Evento onchange desde html (OPCION 3, desde html)
+ //Evento onchange desde html (OPCION 3, desde html)
 //ONCHANGE (detecta cuando se pasa de un campo a otro)
 function activarValidador(entrada) {
     let edadInput = entrada.value;
     if (edadInput < 1 || edadInput > 100) {//Debajo de 1 y superior a 100
         alert("Edad invalida!");
     }
-}
+} 
